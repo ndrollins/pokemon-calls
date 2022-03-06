776cee72f7aa42321424ad7fe7ba6f179800e343
N8 Rollins <nathanield.rollins@gmail.com>
2022-03-06 07:23:20 +0000

    Finished creating tests and the functions with a simple terminal UI

--- lib/poke_call.ex
+++ lib/poke_call.ex
@@ -8,44 +8,71 @@ defmodule PokeCall do
   """
   use Tesla
 
-  plug Tesla.Middleware.BaseUrl, "https://pokeapi.co/api/v2/"
+  plug Tesla.Middleware.BaseUrl, "https://pokeapi.co/api/v2/pokemon/"
   plug Tesla.Middleware.JSON
 
-  def poke_id(poke_name) do
-    id = get("pokemon/" <> poke_name <> "/")
-    case id do
-      {:ok, info} ->
-        poke_list = info.body
-        poke_list["id"]
+  def user_input do
+    choice = IO.gets("Enter pokemon ID [y]es or [n]o: ")
+            |> String.split("\n", trim: true)
+            |> List.to_string
+            |> String.slice(0..0)
+            |> String.downcase()
+    case choice do
+      "y" ->
+        input = IO.gets("Enter pokemon ID #: ")
+            |> String.split("\n", trim: true)
+            |> List.to_string
+            |> String.to_integer()
+        get_name(input)
+      "n" ->
+        choice2 = IO.gets("Enter pokemon name [y]es or [n]o: ")
+            |> String.split("\n", trim: true)
+            |> List.to_string
+            |> String.slice(0..0)
+            |> String.downcase()
+        case choice2 do
+          "y" ->
+            input2 = IO.gets("Enter pokemon name: ")
+                |> String.split("\n", trim: true)
+                |> List.to_string
+            get_id(input2)
+          "n" ->
+            IO.puts("Have a beautiful poke-free day!")
+        end
     end
   end
 
-  def poke_name(poke_id_num) do
-    name =
-      get("pokemon-form/" <> Integer.to_string(poke_id_num) <> "/")
-    case name do
-      {:ok, info} ->
-        poke_list = info.body
-        poke_list["name"]
+  defp get_id(poke_name) do
+    with true <- poke_name_check(poke_name) do
+      {:ok, info} = get(poke_name <> "/")
+      info.body["id"]
+    else
+      _ ->
+        {:error, "Not a valid pokemon name"}
     end
-
   end
 
-defmodule GitHub do
-  use Tesla
+  defp get_name(poke_id_num) do
 
-  plug Tesla.Middleware.BaseUrl, "https://api.github.com"
-  plug Tesla.Middleware.Headers, %{"User-Agent" => "tesla"}
-  plug Tesla.Middleware.JSON
+    case (poke_id_num <= 898 && poke_id_num > 0) do
+      true ->
+        {:ok, info} = get(Integer.to_string(poke_id_num) <> "/")
+        info.body["name"]
 
-  def user_info(login) do
-    get("/user/" <> login)
+      false ->
+        {:error, "Pokemon ID number is invalid"}
+    end
   end
 
-  def user_repos(login) do
-    get("/user/" <> login <> "/repos")
+  defp poke_name_check(poke_name) do
+   all_poke = Enum.reduce(1..898, %{}, fn
+      poke, poke_map ->
+       #%{poke_map | get_name(poke) => poke}
+       Map.put(poke_map, get_name(poke), poke)
+    end)
+
+    Map.has_key?(all_poke, poke_name)
+
+    #poke_name in poke_list
   end
-
-end
-
 end
--- test/poke_call_test.exs
+++ test/poke_call_test.exs
@@ -1,23 +1,40 @@
 defmodule PokeCallTest do
   use ExUnit.Case
-
-  import Tesla.Mock 
+  import Tesla.Mock
 
   setup do
     mock(fn
-      %{method: :get, url: "https://pokeapi.co/api/v2/pokemon/"} ->
-          %Tesla.Env{body: "name"}
+      %{method: :get, url: "https://pokeapi.co/api/v2/pokemon/25"} ->
+        %Tesla.Env{body: %{"name" => "pikachu"}}
 
-        #%{method: :post, url: "https://example.com/world"} ->
-            #json(%{"my" => "data"})
+      %{method: :get, url: "https://pokeapi.co/api/v2/pokemon/0"} ->
+        %Tesla.Env{body: %{"name" => {:error, "Not a valid pokemon id"}}}
+
+      %{method: :get, url: "https://pokeapi.co/api/v2/pokemon/pikachu"} ->
+        %Tesla.Env{body: %{"id" => 25}}
+
+      %{method: :get, url: "https://pokeapi.co/api/v2/pokemon/pikabluu"} ->
+        %Tesla.Env{body: %{"id" => {:error, "Not a valid pokemon name"}}}
+
       end)
 
       :ok
     end
 
   test "for each pokemon name it returns an id, and for each id it returns a name" do
-    #assert PokeCalls.hello() == :world
-    assert PokeCall.poke_id("pikachu") == 25
-    assert PokeCall.poke_name(25) == "pikachu"
+    assert {:ok, %Tesla.Env{} = id} = PokeCall.get("25")
+    assert id.body["name"] == "pikachu"
+
+    assert {:ok, %Tesla.Env{} = id} = PokeCall.get("0")
+    assert id.body["name"] == {:error, "Not a valid pokemon id"}
+  end
+
+  test "gets an id" do
+    assert {:ok, %Tesla.Env{} = name} = PokeCall.get("pikachu")
+    assert name.body["id"] == 25
+
+    assert {:ok, %Tesla.Env{} = id} = PokeCall.get("pikabluu")
+    assert id.body["id"] == {:error, "Not a valid pokemon name"}
+
   end
 end
